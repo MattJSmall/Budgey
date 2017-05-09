2a95d63296cfe7bff7e9da12f471881ca81d6b5d
mjsma5 <mjsma5@student.monash.edu>
2017-05-09 16:57:57 +1000

    pre calc constraints test

--- app/src/main/java/mjsma5/budgey/Login.java
+++ app/src/main/java/mjsma5/budgey/Login.java
@@ -1,9 +1,9 @@
 package mjsma5.budgey;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -201,6 +201,8 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
             btnSignOut.setVisibility(View.VISIBLE);
             btnSignIn.setVisibility(View.VISIBLE);
         }
-
     }
+
+
+
 }
